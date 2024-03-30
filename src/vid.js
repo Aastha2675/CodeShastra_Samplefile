f8b871685304f164edfe707088c5f4c95f0268b6
interstellar201817 <dwijpramodsawant@gmail.com>
2024-03-30 23:00:12 +0530

    update

--- src/vid.js
+++ src/vid.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
 const videos = [
   {
     id: 1,
     title: "Introduction to React",
-    url: "https://black-near-carp-356.mypinata.cloud/ipfs/QmQS6AmN4Hr6iisCtLueKRA2VRFG9F1A8Vjxt1JRXX6Mho?pinataGatewayToken=aRuUl6JVTe85Q4zVeettctJwcI_T9OdNxqduaIMUhqQXN1hiceApdSewljaO6PO9",
+    url: "https://black-near-carp-356.mypinata.cloud/ipfs/QmNuJF3Fsy4bK1anrmcTe9mEx3NK87NHT2vdioacpEa9na?pinataGatewayToken=aRuUl6JVTe85Q4zVeettctJwcI_T9OdNxqduaIMUhqQXN1hiceApdSewljaO6PO9",
     thumbnail: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
     description: "Learn the basics of React framework",
     duration: "10:30"
@@ -14,7 +10,7 @@ const videos = [
   {
     id: 2,
     title: "React Hooks Tutorial",
-    url: "https://black-near-carp-356.mypinata.cloud/ipfs/QmQS6AmN4Hr6iisCtLueKRA2VRFG9F1A8Vjxt1JRXX6Mho?pinataGatewayToken=aRuUl6JVTe85Q4zVeettctJwcI_T9OdNxqduaIMUhqQXN1hiceApdSewljaO6PO9",
+    url: "https://black-near-carp-356.mypinata.cloud/ipfs/QmNuJF3Fsy4bK1anrmcTe9mEx3NK87NHT2vdioacpEa9na?pinataGatewayToken=aRuUl6JVTe85Q4zVeettctJwcI_T9OdNxqduaIMUhqQXN1hiceApdSewljaO6PO9",
     thumbnail: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
     description: "Exploring React hooks like useState and useEffect",
     duration: "15:45"
@@ -58,13 +54,14 @@ const VideoList = ({ videos }) => {
     </div>
   );
 };
-
 function Site() {
   return (
     <div>
+      {/* Call connectWallet function when button is clicked
+      <button onClick={connectWallet}>test</button> */}
       <VideoList videos={videos} />
     </div>
   );
-};
+}
 
 ReactDOM.render(<Site />, document.getElementById('root'));
